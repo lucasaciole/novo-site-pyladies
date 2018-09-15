bbea983d07e52e6d1d1fbf26f032e2fbfdb06808
Lucas Yuji Suguinoshita Aciole <lucas.aciole@gmail.com>
2018-09-15 18:53:49 -0300

    Order alphabetically and split members in 2 lists

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Member
 
+
 # Create your views here.
 def index(request):
-    members = Member.objects.all()
-    return render(request, 'myapp/index.html', {"members": list(members)})
+    members_right = list(Member.objects.order_by('name'))
+    members_left = members_right[0:(len(members_right) // 2)]
+    members_right = members_right[(len(members_right) // 2):(int(len(members_right)))]
+    return render(request, 'myapp/index.html', {"members_left": members_left, "members_right": members_right})
